0a5e968c9f3a9678e308a1f854b887f94312ec6f
Nclear <xyz.games.apps@gmail.com>
2022-02-26 13:02:29 +0100

    Update

--- script.js
+++ script.js
@@ -1,15 +1,4 @@
-function startTime() {
-  const today = new Date();
-  let h = today.getHours();
-  let m = today.getMinutes();
-  let s = today.getSeconds();
-  m = checkTime(m);
-  s = checkTime(s);
-  document.getElementById("time").innerHTML = h + ":" + m + ":" + s;
-  setTimeout(startTime, 1000);
-}
-
-function checkTime(i) {
-  if (i < 10) {i = "0" + i};
-  return i;
+function Time() {
+  document.getElementById("time").innerHTML = new Date().toLocaleTimeString();
+  setTimeout(Time, 1000);
 }
