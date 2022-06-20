debf8eaf6acdf9f70b7a907628d93418cc67b0e4
Nclear <xyz.games.apps@gmail.com>
2022-06-20 23:01:02 +0200

    New version

--- script.js
+++ script.js
@@ -2,3 +2,10 @@ function Time() {
   document.getElementById("time").innerHTML = new Date().toLocaleTimeString();
   setTimeout(Time, 1000);
 }
+
+function msg() {
+  if (confirm("Discord Username: Nclear#0981\nPress OK to open profile")) {
+    window.open('https://discord.com/users/384972072729837570', '_blank').focus();
+  } else {
+  }
+}
