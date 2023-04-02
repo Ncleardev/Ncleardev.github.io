2efbe2dac070101ecb6773897b1df788499f9203
Nclear <xyz.games.apps@gmail.com>
2023-04-03 00:23:02 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function Time() {
+  document.getElementById("time").innerHTML = new Date().toLocaleTimeString();
+  setTimeout(Time, 1000);
+}
+
+function msg() {
+  if (confirm("Discord Username: Nclear#0981\nPress OK to open profile")) {
+    window.open('https://discord.com/users/384972072729837570', '_blank').focus();
+  } else {
+  }
+}
+
+function msg2() {
+  if (confirm("Email: xyz.games.apps@gmail.com\nPress OK to create email draft")) {
+    window.open('mailto:xyz.games.apps@gmail.com', '_blank').focus();
+  } else {
+  }
+}
